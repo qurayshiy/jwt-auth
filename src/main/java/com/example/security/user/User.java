c164cc76a5596b4643d8a31df829022ad1e8999a
qurayshiy <jorayevvabdullohh9@gmail.com>
2023-11-30 16:27:55 +0500

    Added User entity and some project properties

---
+++ src/main/java/com/example/security/user/User.java
@@ -0,0 +1,25 @@
+package com.example.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
