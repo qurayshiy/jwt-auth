a1bbcc0144f8beb069390311eabeb1b8aa907232
qurayshiy <jorayevvabdullohh9@gmail.com>
2023-11-30 17:28:30 +0500

    Added JwtService, JwtAuthFilter and some changes.

---
+++ src/main/java/com/example/security/config/JwtService.java
@@ -0,0 +1,33 @@
+package com.example.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "8z1o/VdRD`O2eeB(#_')PxTh&bBT>EB6l9ZHvLQihzoha@6k3/(`;^IEHsH9;!a";
+
+    public String extractUserEmail(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
